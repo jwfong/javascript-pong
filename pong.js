93eb454ed8039fbbebd2a258b3e9ae05cd5401b0
Joshua Fong <jwfong89@gmail.com>
2016-11-06 10:32:28 -0800

    added score board and net to gameBoard

--- pong.js
+++ pong.js
@@ -14,14 +14,25 @@ const paddleHeight = 100;
 const paddleThickness = 5;
 let framesPerSecond = 30;
 
+let player1score = 0;
+let player2score = 0;
+
+//set number of scores required to win
+const winningScore = 5;
+
 function movement() {
-  ballX = ballX + ballSpeedX;
-  ballY = ballY + ballSpeedY;
+  ballX += ballSpeedX;
+  ballY += ballSpeedY;
   
   if (ballX < 0) {
     if (ballY > paddle1Y && ballY < paddle1Y + paddleHeight) {
       ballSpeedX = -ballSpeedX
+      
+      let deltaY = ballY - (paddle1Y + paddleHeight/2);
+      ballSpeedY = deltaY * 0.35;
+
     } else {
+      player2score++
       ballReset();
     }
   }
@@ -29,7 +40,11 @@ function movement() {
   if (ballX > canvus.width) {
     if (ballY > paddle2Y && ballY < paddle2Y + paddleHeight) {
       ballSpeedX = -ballSpeedX
+
+      let deltaY = ballY - (paddle2Y + paddleHeight/2);
+      ballSpeedY = deltaY * 0.35;
     } else {
+      player1score++;
       ballReset();
     }
   } 
@@ -41,6 +56,16 @@ function movement() {
   if (ballY > canvus.height) {
     ballSpeedY = -ballSpeedY;
   } 
+}
+
+function computerMovement() {
+  let paddle2YCenter = paddle2Y + (paddleHeight/2);
+
+  if (paddle2YCenter < ballY - 35) {
+    paddle2Y += 6;
+  } else if (paddle2YCenter > ballY + 35) {
+    paddle2Y -= 6;
+  }
 }
 
 function calculateMousePos(evt) {
@@ -66,8 +91,21 @@ function drawGameBoard() {
   colorRect(canvus.width - paddleThickness-1, paddle2Y, paddleThickness, paddleHeight, 'white');
   //ball
   drawBall(ballX, ballY, 7, 'white');
+  //display player 1 score
+  canvusContext.fillText("Play 1 Score: " + player1score, 100, 100);
+  //display player 2 score
+  canvusContext.fillText("Play 2 Score: " + player2score, canvus.width - 100, 100);
   
+  drawNet();
+  // colorRect(canvus.width/2, 0, 2, canvus.height,'white');
 };
+
+
+function drawNet() {
+  for(var i = 0; i < canvus.height; i += 40){
+    colorRect(canvus.width/2-1, i, 2, 20, 'white');
+  }
+}
 
 function colorRect(leftX, topY, width, height, color){
   canvusContext.fillStyle = color;
@@ -83,6 +121,11 @@ function drawBall(centerX, centerY, radius, color) {
 }
 
 function ballReset() {
+  if(player1score >= winningScore || player2score >= winningScore) {
+    player1score = 0;
+    player2score = 0;
+  }
+
   ballSpeedX = -ballSpeedX;
   ballX = canvus.width/2;
   ballY = canvus.height/2;
@@ -91,13 +134,12 @@ function ballReset() {
 setInterval(function(){
   movement();
   drawGameBoard();
+  computerMovement();
 }, 1000/framesPerSecond);
 
 canvus.addEventListener('mousemove', function(evt) {
   var mousePos = calculateMousePos(evt);
-  //paddle1Y = mousePos.y - (paddleHeight/2);
-  paddle2Y = mousePos.y - (paddleHeight/2);
-  
+  paddle1Y = mousePos.y - (paddleHeight/2);
 })
 
 
